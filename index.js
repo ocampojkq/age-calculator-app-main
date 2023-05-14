03d4e95255fa0edbe5a1841d29c5592681b0013d
ocampojkq <ocampo.jessekit1@gmail.com>
2023-05-15 00:21:19 +0800

    need to sleep

--- index.js
+++ index.js
@@ -1,39 +1,71 @@
 function calculateAge() {
-  // Get the user's input values for day, month, and year
-  const day = document.getElementById("day").value;
-  const month = document.getElementById("month").value;
-  const year = document.getElementById("year").value;
+  // Get user input
+  const dayInput = document.getElementById("day").value;
+  const monthInput = document.getElementById("month").value;
+  const yearInput = document.getElementById("year").value;
 
-  // Check if the input values are valid
-  if (isNaN(day) || day < 1 || day > 31) {
-    document.querySelector(".days").innerHTML = "Must be a valid day";
+  // Validate user input
+  const validDay = validateDay(dayInput);
+  const validMonth = validateMonth(monthInput);
+  const validYear = validateYear(yearInput);
+  if (!validDay || !validMonth || !validYear) {
     return;
   }
 
-  if (isNaN(month) || month < 1 || month > 12) {
-    document.querySelector(".months").innerHTML = "Must be a valid month";
-    return;
+  // Calculate age
+  const birthday = new Date(`${yearInput}-${monthInput}-${dayInput}`);
+  const today = new Date();
+  const ageInMilliseconds = today - birthday;
+  const ageInYears = Math.floor(ageInMilliseconds / 31556952000); // 31556952000 milliseconds in a year
+  const ageInMonths = Math.floor(
+    (ageInMilliseconds % 31556952000) / 2629746000
+  ); // 2629746000 milliseconds in a month
+  const ageInDays = Math.floor((ageInMilliseconds % 2629746000) / 86400000); // 86400000 milliseconds in a day
+
+  // Display age
+  document.querySelector(".span.years").textContent = ageInYears;
+  document.querySelector(".span.months").textContent = ageInMonths;
+  document.querySelector(".span.days").textContent = ageInDays;
+}
+
+function validateDay(day) {
+  const dayInt = parseInt(day);
+  if (isNaN(dayInt) || dayInt < 1 || dayInt > 31) {
+    document
+      .querySelectorAll(".error-message-day")
+      .forEach((element) => (element.style.display = "block"));
+    return false;
   }
+  document
+    .querySelectorAll(".error-message-day")
+    .forEach((element) => (element.style.display = "none"));
+  return true;
+}
 
-  if (isNaN(year) || year < 1900 || year > new Date().getFullYear()) {
-    document.querySelector(".years").innerHTML = "Must be a valid year";
-    return;
+function validateMonth(month) {
+  const monthInt = parseInt(month);
+  if (isNaN(monthInt) || monthInt < 1 || monthInt > 12) {
+    document
+      .querySelectorAll(".error-message-month")
+      .forEach((element) => (element.style.display = "block"));
+    return false;
   }
+  document
+    .querySelectorAll(".error-message-month")
+    .forEach((element) => (element.style.display = "none"));
+  return true;
+}
 
-  // Get the current date
-  const currentDate = new Date();
-
-  // Calculate the user's birth date from the input values
-  const birthDate = new Date(`${year}-${month}-${day}`);
-
-  // Calculate the user's age
-  const ageInMilliseconds = currentDate - birthDate;
-  const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
-  const ageInMonths = ageInYears * 12;
-  const ageInDays = ageInMilliseconds / (1000 * 60 * 60 * 24);
-
-  // Display the calculated age in the UI
-  document.querySelector(".years").innerHTML = Math.floor(ageInYears);
-  document.querySelector(".months").innerHTML = Math.floor(ageInMonths);
-  document.querySelector(".days").innerHTML = Math.floor(ageInDays);
+function validateYear(year) {
+  const yearInt = parseInt(year);
+  if (isNaN(yearInt) || yearInt < 1900 || yearInt > new Date().getFullYear()) {
+    document
+      .querySelectorAll(".error-message-year")
+      .forEach((element) => (element.style.display = "block"));
+    return false;
+  }
+  document
+    .querySelectorAll(".error-message-year")
+    .forEach((element) => (element.style.display = "none"));
+  return true;
 }
