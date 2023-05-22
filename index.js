fb9e9677721264efa1c6dd98b3545a41772e20d5
ocampojkq <ocampo.jessekit1@gmail.com>
2023-05-22 21:27:53 +0800

    adding images

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 function answer(event) {
   event.preventDefault();
 
+  var userInputDay = document.getElementById("day").value; // Get the user input day
+  var userInputMonth = document.getElementById("month").value; // Get the user input month
+  var userInputYear = document.getElementById("year").value; // Get the user input year
+
+  var targetDay = 12; // The target day
+  var targetMonth = 11; // The target month
+  var targetYear = 2014; // The target year
+
+  // Check if the user input matches the target date
+  if (
+    userInputDay === targetDay.toString() &&
+    userInputMonth === targetMonth.toString() &&
+    userInputYear === targetYear.toString()
+  ) {
+    document.getElementById("carmelo").style.display = "block"; // Display the hidden element
+  } else {
+    document.getElementById("carmelo").style.display = "none"; // Hide the element if the input doesn't match
+  }
+
+  var userInputDay1 = document.getElementById("day").value; // Get the user input day
+  var userInputMonth1 = document.getElementById("month").value; // Get the user input month
+  var userInputYear1 = document.getElementById("year").value; // Get the user input year
+
+  var targetDay1 = 22; // The target day
+  var targetMonth1 = 03; // The target month
+  var targetYear1 = 2010; // The target year
+
+  // Check if the user input matches the target date
+  if (
+    userInputDay1 === targetDay1.toString() &&
+    userInputMonth1 === targetMonth1.toString() &&
+    userInputYear1 === targetYear1.toString()
+  ) {
+    document.getElementById("keane").style.display = "block"; // Display the hidden element
+  } else {
+    document.getElementById("keane").style.display = "none"; // Hide the element if the input doesn't match
+  }
+
   const day = document.getElementById("day").value;
   const month = document.getElementById("month").value;
   const year = document.getElementById("year").value;
